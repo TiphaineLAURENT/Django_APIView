a82221ee4ef0c929999851ae0bfd8da4c764ff36
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-08-14 17:10:38 +0200

    Fix fk assignment

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='django_modelapiview',
-    version='1.1.1',
+    version='1.1.2',
     url='https://github.com/TiphaineLAURENT/Django_APIView',
     license='BSD',
     author='Tiphaine LAURENT',
