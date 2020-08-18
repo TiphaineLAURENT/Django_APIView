35fbc8e169ed4ec92b348b160bfacf65818c7bb4
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-08-18 22:24:36 +0200

    Update version to 1.3.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='django_modelapiview',
-    version='1.2.1',
+    version='1.3.0',
     url='https://github.com/TiphaineLAURENT/Django_APIView',
     license='BSD',
     author='Tiphaine LAURENT',
