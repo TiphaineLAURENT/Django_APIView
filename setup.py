17b2daa93fda331056e0cac96165329973ed6bfc
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-09-10 00:16:16 +0200

    Change version to 1.4.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='django_modelapiview',
-    version='1.4.1',
+    version='1.4.2',
     url='https://github.com/TiphaineLAURENT/Django_APIView',
     license='BSD',
     author='Tiphaine LAURENT',
