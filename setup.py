e92827f392e474a3bb1141b2707e3b3bd0dc74a7
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-10-07 23:32:19 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='django_modelapiview',
-    version='1.4.7',
+    version='1.4.8',
     url='https://github.com/TiphaineLAURENT/Django_APIView',
     license='BSD',
     author='Tiphaine LAURENT',
