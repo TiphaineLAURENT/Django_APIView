6cf8f6586ba88cbdf1331c59cbd8a20188ee9854
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-10-07 19:05:08 +0200

    Fix LoginView

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='django_modelapiview',
-    version='1.4.4',
+    version='1.4.5',
     url='https://github.com/TiphaineLAURENT/Django_APIView',
     license='BSD',
     author='Tiphaine LAURENT',
